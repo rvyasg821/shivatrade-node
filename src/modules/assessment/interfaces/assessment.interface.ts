4b6c87cc4016c54b0bbe4cdd3f5547ebb44ba673
JugalMahida <jugal.strivedge@gmail.com>
2026-07-03 14:58:04 +0530

    Delete the Assessment Module

--- src/modules/assessment/interfaces/assessment.interface.ts
+++
@@ -1,9 +0,0 @@
-import { AssessmentEntity } from '@modules/assessment/repository/entities/assessment.entity';
-
-export interface IAssessmentEntity extends Omit<AssessmentEntity, '_id'> {
-    _id: string;
-}
-
-export interface IRequestAssessmentApp {
-    __assessment: IAssessmentEntity;
-}
